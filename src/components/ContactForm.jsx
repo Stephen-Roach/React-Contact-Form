acace6730c5fb0bc4bbe76752c7f170abaacc71e
Stephen-Roach <stephenroach21@gmail.com>
2021-09-03 17:07:25 -0700

    added name section on form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button, FormControl } from 'react-bootstrap';
 
 function ContactForm() {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  function sendEmail(e) {
+    e.preventDefault();
+    console.log('Checking to see if this works!');
+  }
+
+  function onInputChange(e) {
+    const { name, value } = e.target;
+
+    setState({
+      ...state,
+      [name]: value,
+    });
+  }
+
   return (
     <div>
-      <h2>Contact Form</h2>
+      <form onSubmit='sendEmail'>
+        <Form.Group controlId='name'>
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control
+            type='text'
+            name='name'
+            value={state.name}
+            placeholder='Enter your Full Name'
+            onChange={onInputChange}
+          />
+        </Form.Group>
+      </form>
     </div>
   );
 }
